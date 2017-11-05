1dd689a72118a792501ebfbf589ca1c8bb95cd7c
Neeraj Asthana <nasthan2@illinois.edu>
2017-11-05 16:39:39 -0500

    sending debugging data for messaging

--- twilio/messaging.py
+++ twilio/messaging.py
@@ -1,7 +1,7 @@
 import sys
 
 from flask import Flask, request, redirect
-from twilio.twiml.messaging_response import MessagingResponse
+from twilio import twiml
 
 app = Flask(__name__)
 
@@ -9,7 +9,10 @@ app = Flask(__name__)
 def sms():
     number = request.form['From']
     message_body = request.form['Body']
- 
+    
+    print("got here")
+    print(number, message_body)
+
     resp = twiml.Response()
     resp.message('Hello {}, you said: {}'.format(number, message_body))
     print('Hello {}, you said: {}'.format(number, message_body))
