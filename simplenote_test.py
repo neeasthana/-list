2c5f0b251a1339d8647f94a0bfd80e02d888a03d
Neeraj Asthana <nasthan2@illinois.edu>
2017-11-06 23:40:42 -0500

    added message parsing

--- simplenote_test.py
+++ simplenote_test.py
@@ -3,12 +3,22 @@ import simplenote
 username = "neeasthana@gmail.com"
 password = "Waterbottle94"
 
-simplenote = simplenote.Simplenote(username, password)
+#simplenote = simplenote.Simplenote(username, password)
 
-print("got here")
+#result = simplenote.get_note_list()
 
-result = simplenote.get_note_list()
+#print(result)
 
-print(result)
+#print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
 
-print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
+def test(message_body):
+    message_split = message_body.split(" ")
+    if message_split[0][0] is "@":
+        list_name = message_split[0][1:]
+        message_body = " ".join(message_split[1:])
+        print(list_name, message_body)
+    else:
+        print("Error. Message does not start with a list name")
+
+if __name__ == '__main__':
+    test("@list something else is here")
--- twilio/messaging.py
+++ twilio/messaging.py
@@ -1,15 +1,31 @@
 import sys
-
+import simplenote
 from flask import Flask, request, redirect
 from twilio.twiml.messaging_response import Body, Message, Redirect, MessagingResponse
 
 app = Flask(__name__)
 
+username = "neeasthana@gmail.com"
+password = "Waterbottle94"
+
 @app.route('/sms', methods=['POST'])
 def sms():
+    # Get message parameters - number and message
     number = request.form['From']
     message_body = request.form['Body']
     
+    # Establish connection to simplenote
+    simplenote = simplenote.Simplenote(username, password)
+
+    # Parse out list name using "@" annotation
+    message_split = message_body.split(" ")
+    if message_split[0][0] is "@":
+        list_name = message_split[0][1:]
+        message_body = message_split[1:].join(" ")
+    else: 
+        print("Error. Message does not start with a list name")
+
+    # Send response
     resp = MessagingResponse()
     resp.message('Hello {}, you said: {}'.format(number, message_body))
     print('Hello {}, you said: {}'.format(number, message_body))
