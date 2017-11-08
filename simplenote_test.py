10a8c3a833e2cc7ff781a28949088123f35126ce
Neeraj Asthana <nasthan2@illinois.edu>
2017-11-07 21:26:36 -0500

    parsed json from simplenote to get list ids and list contents

--- simplenote_test.py
+++ simplenote_test.py
@@ -3,13 +3,15 @@ import simplenote
 username = "neeasthana@gmail.com"
 password = "Waterbottle94"
 
-#simplenote = simplenote.Simplenote(username, password)
+"""
+simplenote = simplenote.Simplenote(username, password)
 
-#result = simplenote.get_note_list()
+result = simplenote.get_note_list()
 
-#print(result)
+print(result)
 
-#print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
+print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
+"""
 
 def test(message_body):
     message_split = message_body.split(" ")
@@ -21,4 +23,14 @@ def test(message_body):
         print("Error. Message does not start with a list name")
 
 if __name__ == '__main__':
-    test("@list something else is here")
+    #test("@list something else is here")
+    simplenote = simplenote.Simplenote(username, password)
+
+    result = simplenote.get_note_list()
+
+    for entry in result[0]:
+        print(entry['deleted'], entry["key"])
+        if entry["deleted"] == 0:
+            print(simplenote.get_note(entry["key"]))
+        print()
+    #print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
