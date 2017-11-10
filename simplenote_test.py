8e5609a7d2db621a18d06395a944ed2bb0b3dfff
Neeraj Asthana <nasthan2@illinois.edu>
2017-11-09 23:33:35 -0500

    completed reading list names and adding tag names to parse from

--- simplenote_test.py
+++ simplenote_test.py
@@ -1,11 +1,12 @@
 import simplenote
+import json
 
 username = "neeasthana@gmail.com"
 password = "Waterbottle94"
 
-"""
-simplenote = simplenote.Simplenote(username, password)
 
+simplenote = simplenote.Simplenote(username, password)
+"""
 result = simplenote.get_note_list()
 
 print(result)
@@ -13,24 +14,47 @@ print(result)
 print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
 """
 
+
+
+
+def profile_lists(message_body):
+    lists_split = message_body.split("\n\n\n\n")
+    for split in lists_split:
+        print(split.split("\n")[0])
+    return None
+
+    
+
+def add_keys_and_list_names():
+    result = simplenote.get_note_list()
+    with open('my_dict.json') as f:
+        my_dict = json.load(f)
+        for entry in result[0]:
+            if entry["deleted"] == 0:
+                note_id = entry["key"]
+                print(note_id)
+                message_body = simplenote.get_note(note_id)[0]["content"]
+                list_names = profile_lists(message_body)
+                my_dict[note_id] = list_names
+                print()
+        print(my_dict)
+
+
+
 def test(message_body):
     message_split = message_body.split(" ")
     if message_split[0][0] is "@":
-        list_name = message_split[0][1:]
         message_body = " ".join(message_split[1:])
-        print(list_name, message_body)
+        
+        lists_split = message_body.split("\n\n\n")
+
+
+
     else:
         print("Error. Message does not start with a list name")
 
 if __name__ == '__main__':
     #test("@list something else is here")
-    simplenote = simplenote.Simplenote(username, password)
 
-    result = simplenote.get_note_list()
-
-    for entry in result[0]:
-        print(entry['deleted'], entry["key"])
-        if entry["deleted"] == 0:
-            print(simplenote.get_note(entry["key"]))
-        print()
+    add_keys_and_list_names()
     #print(simplenote.get_note("a31b6fa882c94c61ba53c52e0230798c")[0]["content"])
